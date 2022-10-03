7fd286cbd2597adb8e2a65115647fb915a93adb1
bujosa <davidbujosa@gmail.com>
2022-10-02 21:30:03 -0400

    feat: replace text for icon widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() {
   var app = MaterialApp(
       home: Scaffold(
           floatingActionButton: FloatingActionButton(
-            child: const Text('+'),
+            child: const Icon(Icons.add),
             onPressed: () {},
           ),
           appBar: AppBar(title: const Text('First Menu'))));
